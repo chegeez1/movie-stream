1f4f3ba3ca85366bbdb3cb848ba80e23ef52185a
chegeez1 <johnmwangi2159@gmail.com>
2025-11-11 02:18:27 -0900

    Delete src directory

--- src/constants/routes.js
+++
@@ -1,9 +0,0 @@
-export const ROUTES = {
-  HOME: '/',
-  SEARCH: '/search',
-  MOVIE_DETAIL: '/movie/:id',
-  TRENDING: '/trending',
-  DOWNLOADS: '/downloads',
-  FAVORITES: '/favorites',
-  HISTORY: '/history'
-}
