73b8c99f3d8e85f023cde73a313364f0c61366f1
chegeez1 <johnmwangi2159@gmail.com>
2025-11-10 23:49:57 -0900

    Create routes.js

---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  HOME: '/',
+  SEARCH: '/search',
+  MOVIE_DETAIL: '/movie/:id',
+  TRENDING: '/trending',
+  DOWNLOADS: '/downloads',
+  FAVORITES: '/favorites',
+  HISTORY: '/history'
+}
